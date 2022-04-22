ad168ea1f2cb6ed289ec6bd9d714ef4665f5a2ba
TickyWill <amal.chabli@orange.fr>
2022-04-22 14:46:09 +0200

    Added the  module BiblioParsingConcat.py to merge, by concatenation and deduplication, two different corpuses including mandatory normalization of journal names and document types

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ install_requires = open(path.join(this_directory, 'requirements.txt'), encoding=
 # This setup is suitable for "python setup.py develop".
 
 setup(name='BiblioAnalysis_Utils',
-      version='2.2.1',
+      version='3.0.0',
       description='A toolbox for bibliographic analysis',
       long_description=long_description,
       long_description_content_type='text/markdown',
